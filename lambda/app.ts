443503a5c29e2a38f07eefbea490c3619df91154
Jonathan Cummins <jonathan.cummins@bmw.de>
2023-02-25 13:21:00 +0800

    A working http request

--- lambda/app.ts
+++ lambda/app.ts
@@ -1,32 +1,15 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import fetch from 'node-fetch';
 
-export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
 
-    
-    const response = await fetch('https://api.github.com/users/github');
+
+
+export const lambdaHandler = async (event) => {
+    console.log(event);
+    console.log("####################################");
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
     const data = await response.json();
-    
     console.log(data);
-    
-    
-    
-    
-    
-    try {
-        return {
-            statusCode: 200,
-            body: JSON.stringify({
-                message: 'hello world',
-            }),
-        };
-    } catch (err) {
-        console.log(err);
-        return {
-            statusCode: 500,
-            body: JSON.stringify({
-                message: 'some error happened',
-            }),
-        };
-    }
+    console.log("####################################");
+    return "Done"
 };
