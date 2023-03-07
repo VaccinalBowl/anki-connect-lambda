fc856b5b8ebe2cb9fd5e20352269fcdc7029c63a
Jonathan Cummins <jonathan.cummins@bmw.de>
2023-03-08 01:01:26 +0800

    First working version

--- handlers/hello-from-lambda.mjs
+++
@@ -1,8 +0,0 @@
-
-
-import {pinyinify} from 'hanzi-tools';
-
-export const helloFromLambdaHandler = async () => {
-    console.info(pinyinify("你好！你今天吃饭了"));
-    return "Done";
-}
--- src/handlers/anki-connect-lambda.mjs
+++ src/handlers/anki-connect-lambda.mjs
@@ -1,9 +1,8 @@
-
-
+import fetch from 'node-fetch';
 import {pinyinify} from 'hanzi-tools';
-/*
-function checkForSingleCharactersWithoutACard() {
 
+function checkForSingleCharactersWithoutACard() {
+    
     console.log(process.env.ANKI_CONNECT_URL);
     console.log(process.env.ANKI_CONNECT_PORT);
     let endpoint = 'http://' + process.env.ANKI_CONNECT_URL + ':' + process.env.ANKI_CONNECT_PORT;
@@ -16,9 +15,11 @@ function checkForSingleCharactersWithoutACard() {
         headers: requestHeaders,
         body: requestBody
     };
-
+    
     fetch(endpoint, requestOptions)
-        .then(response => response.json())
+        .then(response => {
+            return response.json()
+        })
         .then(ankiFindCardsResponse => {
             var cardIdArray = ankiFindCardsResponse.result;
             var cardInfoRequestBody = JSON.stringify({ "action": "cardsInfo", "version": 6, "params": { "cards": cardIdArray } });
@@ -32,14 +33,14 @@ function checkForSingleCharactersWithoutACard() {
         .then(cardInfoResponse => cardInfoResponse.json())
         .then(cardInfoResponseJson => {
             var ankiCardArray = cardInfoResponseJson.result;
-            let singleCharacterMap = new Map<string, AnkiCard>();
-            let multipleChararacterCards: AnkiCard[] = [];
+            let singleCharacterMap = new Map();
+            let multipleChararacterCards = [];
             let singleCardCount = 0;
             let doubleCardCount = 0;
             console.log(`There are a total of ${ankiCardArray.length} cards`);
 
-            ankiCardArray.forEach((ankiCard: AnkiCard) => {
-                let chinese: string = ankiCard.fields.Front.value;
+            ankiCardArray.forEach((ankiCard) => {
+                let chinese = ankiCard.fields.Front.value;
                 if (chinese.length == 1) {
                     singleCharacterMap.set(chinese, ankiCard);
                     singleCardCount++;
@@ -52,12 +53,12 @@ function checkForSingleCharactersWithoutACard() {
             console.log(`There are a total of ${doubleCardCount} double character cards`);
 
 
-            multipleChararacterCards.forEach((multiCharacterCard: AnkiCard) => {
-                let chinese: string = multiCharacterCard.fields.Front.value;
+            multipleChararacterCards.forEach((multiCharacterCard) => {
+                let chinese = multiCharacterCard.fields.Front.value;
                 chinese.split('').forEach(chineseCharacter => {
                     if (!singleCharacterMap.has(chineseCharacter)) {
-                        //pinyinify(chineseCharacter);
-                        console.log(`${chineseCharacter} needs to be added.`)
+                        var pinyin = pinyinify(chineseCharacter);
+                        console.log(`${chineseCharacter},,${pinyin}`)
                     }
                 });
 
@@ -66,12 +67,12 @@ function checkForSingleCharactersWithoutACard() {
 
         )
         .catch(error => console.log('error', error));
+        
 
 
+}
 
-}*/
-
-export const helloFromLambdaHandler = async () => {
-    console.info(pinyinify("你好！你今天吃饭了"));
+export const ankiConnectLambda = () => {
+    checkForSingleCharactersWithoutACard();
     return "Done";
 }
